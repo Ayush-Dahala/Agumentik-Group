64745fa502d5bb831d8d7fb339adc19cc2a904d5
Ayush Dahala <ayushdahala4@gmail.com>
2022-09-04 09:35:23 +0530

    initial commit

--- src/components/Elements/Callus.jsx
+++ src/components/Elements/Callus.jsx
@@ -7,21 +7,18 @@ class Callus extends React.Component {
     render() {
         return (
             <>
-                <div className="section-full mobile-page-padding p-tb80 overlay-wraper bg-cover bg-center" style={{backgroundImage:"url(" + bnr1.default + ")"}}>
-                    <div className="overlay-main bg-primary opacity-07" />
-                    <div className="container">
-                        <div className="section-content">
-                            <div className="call-us-section text-center" style={{display:'flex'}}>
-                               <div style={{width:'32%' ,background:'#FFFFFF',padding:'30px',marginBottom:30,marginRight:27}}>
+                <div className="container section-full mobile-page-padding p-tb80 overlay-wraper bg-cover bg-center" style={{backgroundImage:"url(" + bnr1.default + ")"}}>
+                    <div className="row overlay-main bg-primary opacity-07" style={{display:'flex' ,textAlign:'center'}}/>
+                               <div className="col-lg-4 col-md-6 col-sm-6 " style={{ background:'#FFFFFF',padding:'30px',marginBottom:30,marginRight:30,height:500}}>
                                 <p style={{fontSize:28,fontWeight:500}}>Contact <b>Agumentik</b></p>
                                 <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.there are many Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde variations of passages of Ipsum available,</p>
                                 <h3>(+291)-456-789-1234</h3>
                                 <NavLink to="/contactus" className="site-button-secondry btn-effect bg-dark">Contact Us</NavLink>
                                 </div>
-                                <div className='h-100' style={{width:'32%' ,background:'#FFFFFF',padding:'30px',marginBottom:30,marginRight:27}}>
+                                <div className="col-lg-3 col-md-6 col-sm-6" style={{background:'#FFFFFF',padding:'30px',marginBottom:30,marginRight:30,height:500}}>
                                         <h1>720 x 960</h1>
                                 </div >
-                                <div className='h-100' style={{width:'32%' ,background:'#FFFFFF',padding:'30px',marginBottom:30,}}>
+                                <div className="col-lg-4 col-md-6 col-sm-6" style={{background:'#FFFFFF',padding:'30px',marginBottom:30,height:500}}>
                                 <p style={{fontSize:28,fontWeight:500}}>Let's work  <b>Together</b></p>
                                 <p>It is a long established fact that a reader will be distracted by the readable content of Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing .</p>
                                 <h3>(+291)-456-789-1234</h3>
@@ -32,9 +29,8 @@ class Callus extends React.Component {
                                 </div>
 
                                 </div>
-                            </div>
-                        </div>
-                    </div>
+=                        
+                  
                 </div>
             </>
         );
